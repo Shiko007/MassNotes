2d042fe1d31c477d1c2974555d0a8b136d8a16be
Shiko <sherifyasser92@gmail.com>
2022-07-31 17:14:28 +0200

    Initial Commit

---
+++ MassNotes/MassNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MassNotesApp.swift
+//  MassNotes
+//
+//  Created by Sherif Yasser on 31.07.22.
+//
+
+import SwiftUI
+
+@main
+struct MassNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
